94b11f8f36bd26627cc887ace3bc8939136f4d47
suusan11 <gawow11@gmail.com>
2019-11-03 17:00:06 -0800

    pagination.php add to template parts

--- single-gallery.php
+++ single-gallery.php
@@ -22,27 +22,7 @@ get_header();
     <?php the_content(); ?>
   </div>
 
-  <div class="link__posts">
-    <?php
-        $prev_post = get_previous_post();
-        if (!empty($prev_post)):
-            ?>
-    <div class="prev">
-      <p>Previous Post</p>
-      <h2><?php previous_post_link('%link', '%title'); ?>
-      </h2>
-    </div>
-    <?php endif; ?>
-    <div class="next">
-      <?php
-        $next_post = get_next_post();
-        if (!empty($next_post)):
-            ?>
-      <p>Next Post</p>
-      <h2><?php next_post_link('%link', '%title'); ?>
-    </div>
-    <?php endif; ?>
-  </div>
+  <?php get_template_part('template-parts/content', 'pagination')?>
 </main>
 
 <?php
--- single.php
+++ single.php
@@ -60,27 +60,8 @@ foreach ((get_the_category()) as $cat) {
 		<div class="border__top"></div>
 		<div class="border__bottom"></div>
 	</article>
-	<div class="link__posts">
-		<?php
-        $prev_post = get_previous_post();
-        if (!empty($prev_post)):
-            ?>
-		<div class="prev">
-			<p>Previous Post</p>
-			<h2><?php previous_post_link('%link', '%title'); ?>
-			</h2>
-		</div>
-		<?php endif; ?>
-		<div class="next">
-			<?php
-        $next_post = get_next_post();
-        if (!empty($next_post)):
-            ?>
-			<p>Next Post</p>
-			<h2><?php next_post_link('%link', '%title'); ?>
-		</div>
-		<?php endif; ?>
-	</div>
+
+	<?php get_template_part('template-parts/content', 'pagination')?>
 </main>
 
 <?php
